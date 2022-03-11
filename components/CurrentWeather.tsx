afa8626ddaf136bb38f39aec133c9fa92956c9e4
Matthieu <ribeyrolles.matthieu@gmail.com>
2022-03-11 11:08:27 +0100

    ui changes for current weather

--- components/CurrentWeather.tsx
+++ components/CurrentWeather.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useCurrentWeather } from '../hooks/useCurrentWeather';
 import { useAppSelector } from '../redux/app/hooks';
-import { initialState } from '../redux/features/preferencesSlice';
-import { convert } from '../utils/convertor';
 import { getTemp } from '../utils/temp';
 import styles from './../styles/CurrentWeather.module.scss';
+import card from './../styles/Card.module.scss';
 /**
  * The element displaying the current weather data.
  * Fetches data and renders depending of it.  
@@ -15,31 +14,34 @@ export const CurrentWeather = () => {
   const city = useAppSelector(state => state.city.name);
   const { currentWeather, loading, error } = useCurrentWeather(city);
 
-  if (error) return <>An error has occured.</>
-
   return (
-    <div className={styles.container}>
-      {loading && <>..</>} 
-      {currentWeather && 
+    <div className={`${card.card__big}`}>
+      <div className={styles.date}>
+        Today
+        <span>{new Date().toLocaleDateString()}</span>  
+      </div>
 
-        <div className={styles.currentWeather}>
-          <span className={styles.description}> 
-            {currentWeather.weather.description} 
+      <div className={styles.weather}>
+        {error && <>Whoops. Something went wrong.</>}
+        {loading && <span>..</span>}
+        {currentWeather && 
+          <>
+            <span className={styles.temp}>
+              {getTemp(currentWeather.main.temp, measureUnit)} <span className={styles.units}>{measureUnit}  </span>
+            </span>
+
             <img src={`http://openweathermap.org/img/wn/${currentWeather.weather.icon}@2x.png`} alt="" />
-          </span>
+          </>  
+        }
+      </div>
+      <div className={styles.location}>
+        <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M11.25 6C11.25 9.75 6 13.25 6 13.25C6 13.25 0.75 9.75 0.75 6C0.75 4.60761 1.30312 3.27226 2.28769 2.28769C3.27226 1.30312 4.60761 0.75 6 0.75C7.39239 0.75 8.72774 1.30312 9.71231 2.28769C10.6969 3.27226 11.25 4.60761 11.25 6V6Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+          <path d="M6 7.25C6.69036 7.25 7.25 6.69036 7.25 6C7.25 5.30964 6.69036 4.75 6 4.75C5.30964 4.75 4.75 5.30964 4.75 6C4.75 6.69036 5.30964 7.25 6 7.25Z" fill="white" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+        </svg>
 
-
-          <div className={styles.city}> {city.toUpperCase()} </div>
-            
-          <div className={styles.currentTemp}>
-            <span>{getTemp(currentWeather.main.temp, measureUnit)}</span>
-            <span className={styles.upper}> {measureUnit} </span>
-          </div>
-
-          <div> Feels like: {loading && <>Loading..</>} {getTemp(currentWeather.main.feels_like, measureUnit) + measureUnit} </div>
-        </div>
-      }
-
+        {city}
+      </div>
     </div>
   )
 }
